b6fe8aad90c37d914b0cf69d4421dfda3ff69967
Sandeepmehra79 <83429487+Sandeepmehra79@users.noreply.github.com>
2024-05-03 05:21:21 +0530

    Delete HelloWorld/company/backendLLDProject/march24/productService directory

--- HelloWorld/company/backendLLDProject/march24/productService/demo/src/main/java/com/product/demo/Exception/LimitTooLargeException.java
+++
@@ -1,7 +0,0 @@
-package com.product.demo.Exception;
-
-public class LimitTooLargeException extends Exception{
-    public LimitTooLargeException(String message){
-        super(message);
-    }
-}
--- HelloWorld/company/backendLLDProject/march24/productService/demo/src/main/java/com/product/demo/Exception/NoProductInListException.java
+++
@@ -1,7 +0,0 @@
-package com.product.demo.Exception;
-
-public class NoProductInListException extends Exception{
-    public NoProductInListException(String message){
-        super(message);
-    }
-}
